102e28d353939dcd6ea75b9224157b3c4c4a9a64
Sujan Rao Chikkela <sujanraochikkela@gmail.com>
2022-05-17 23:40:16 -0700

    query for frontend graphql

---
+++ client/query (1).js
@@ -0,0 +1,50 @@
+import { gql } from "@apollo/client";
+
+export const LOAD_PRODUCTS = gql`
+  query {
+    getItemsList {
+      _id
+      userId
+      itemCategory
+      itemName
+      itemPrice
+      itemDescription
+      itemCount
+      itemImage
+    }
+  }
+`;
+
+export const LOAD_CART_ITEMS = gql`
+  query getCartList($userId: String!) {
+    getCartList(userId: $userId) {
+      userId
+      qty
+      itemId {
+        _id
+        itemCategory
+        itemName
+        itemPrice
+        itemDescription
+        itemCount
+        itemImage
+      }
+    }
+  }
+`;
+
+export const GET_PURCHASES_LIST = gql`
+  query get($userId: String!) {
+    getCartList(userId: $userId) {
+      userId
+      qty
+      _id
+      itemCategory
+      itemName
+      itemPrice
+      itemDescription
+      itemCount
+      itemImage
+    }
+  }
+`;
